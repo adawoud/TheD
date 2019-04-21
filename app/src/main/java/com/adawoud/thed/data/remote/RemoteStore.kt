93135a90e19b78ab31790b4be6def317a880f0dd
adawoud <adawoud7@gmail.com>
2019-04-21 21:18:34 +0200

    Add proper documentation to explain why we get the product details the way we do

--- app/src/main/java/com/adawoud/thed/data/remote/RemoteStore.kt
+++ app/src/main/java/com/adawoud/thed/data/remote/RemoteStore.kt
@@ -11,6 +11,12 @@ class RemoteStore @Inject constructor(
     fun products() = theDService.products()
         .map { response -> response.data }
 
+
+    /**
+     * We could use the same endpoint above for getting the product details, and pass
+     * them to the Detail screen using arguments since they're only a few text fields,
+     * but this solution is more general, and also future-proof.
+     * */
     fun productDetails() = theDService.products()
         .map { response -> response.data }
 
